a9d52373c260fa52429bb8ad5cade9132ead42c9
Blake Woolbright <blake.woolbright@gmail.com>
2015-12-28 22:01:44 -0800

    Added some extended reference material I downloaded

---
+++ external/cmake-modules/nlohmann-json.cmake
@@ -0,0 +1,6 @@
+fps_add_external_component( 
+    LABEL                "nlohmann-json"
+    DESCRIPTION          "JSON C++ Library by Niels Lohmann (https://github.com/nlohmann/json)"
+    INCLUDE_DIRS         "/home/bwoolbright/dev/fps_toolchain/nlohmann-json/versions/nlohmann-json-1.0.0/include"
+    PROVIDES             ""
+)
