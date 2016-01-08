a60dee3117a80a85f0908e676c335eb935f60ff0
Blake Woolbright <blake.woolbright@gmail.com>
2016-01-07 23:00:57 -0800

    fps_ipc :: swmr::RingBuffer<> example now has working reader and writer, but I haven't tested concurrent execution yet.

--- cpp/examples/ipc/swmr_queue.reader_example.cpp
+++ cpp/examples/ipc/swmr_queue.reader_example.cpp
@@ -14,30 +14,28 @@ main( int argc, char * argv[] )
   typedef ipc::swmr::RingBuffer<msg_t, examples::ipc::swmr::Capacity> rb_t ;
 
   fs::Path shm_path( "/dev/shm", examples::ipc::swmr::Queue_Name ) ;
-  shm_path.rm() ;
+  if( !shm_path.exists() ) 
+  { std::cout << "Error :: Failed to find shm segment @ '" << shm_path.str() << "'" << std::endl 
+              << std::endl ;
+    return 1 ;
+  }
 
   ipc::SharedMemory shm ;
-  if( !shm.open( shm_path.leaf(), ipc::access::Create | ipc::access::Read_Write ) ) 
+  if( !shm.open( shm_path.leaf(), ipc::access::Read_Only ) ) 
   { std::cout << "Error :: Failed to open shared memory segment w/ name '" 
               << shm_path.str() << "', capacity: " << rb_t::Capacity << std::endl 
               << std::endl ;
     return 1 ;
   }
   
-  if( !shm.resize<rb_t>() ) 
-  { std::cout << "Error :: Failed to resize shm segment to " << sizeof( rb_t ) << " bytes" << std::endl 
-              << std::endl ;
-    return 1 ;
-  }
-
   ipc::MappedMemory shm_map ; 
-  if( !shm_map.open( shm, ipc::access::Read_Write ) ) 
-  { std::cout << "Error :: Failed to create read-write memory map on shared memory segment" << std::endl 
+  if( !shm_map.open( shm, ipc::access::Read_Only ) ) 
+  { std::cout << "Error :: Failed to create read-only memory map on shared memory segment" << std::endl 
               << std::endl ;
     return 1 ;
   }
 
-  std::cout << "[ fps::examples::ipc::swmr_queue - writer ]" << std::endl   
+  std::cout << "[ fps::examples::ipc::swmr_queue - reader ]" << std::endl   
             << "|--[ rb_t::Capacity : " << rb_t::Capacity << " ]" << std::endl 
             << "|--[ shm.size()     : " << shm.size() << " ]" << std::endl 
             << "|--[ MMap.size()    : " << shm_map.size() << " ]" << std::endl 
@@ -45,7 +43,7 @@ main( int argc, char * argv[] )
             << "|--[ sizeof( rb_t ) : " << sizeof( rb_t ) << " ]" << std::endl 
             << "|" << std::endl ;
   
-  rb_t * swmr_rb = shm_map.construct<rb_t>() ;
+  rb_t * swmr_rb = shm_map.cast<rb_t>() ;
 
   if( !swmr_rb ) 
   { std::cout << "Error :: Failed to construct swmr_queue in mapped shm segment (errno: " << errno << ")" << std::endl ;
@@ -57,12 +55,17 @@ main( int argc, char * argv[] )
             << "|" << std::endl ;
 
   msg_t msg ;
-  for( uint32_t value = 1 ; value < rb_t::Capacity ; ++value ) 
-  { msg.set( value ) ;
-    swmr_rb->write( msg ) ;
+  uint32_t idx = 0 ;
+  for( ; idx < rb_t::Capacity ; ++idx ) 
+  { 
+    if( !swmr_rb->read( idx, msg ) ) 
+      break ;
+  
+    std::cout << "|--[ " << idx << " : " << msg.get() << " ]" << std::endl ;
   }
-
-  ::usleep( 1000 ) ;
+  std::cout << "|" << std::endl 
+            << "|--[ Reads : " << idx + 1 << " ]" << std::endl 
+            << "|" << std::endl ;
 
   std::cout << std::endl ;
   
