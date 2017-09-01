56a8ee4bc2eb491a642c4e823da143ad0dba8744
Blake Woolbright <blake.woolbright@gmail.com>
2017-08-31 22:33:54 -0700

    Function serialization

--- python/app/py2cpp/py2cpp.function.test.py
+++ python/app/py2cpp/py2cpp.function.test.py
@@ -8,6 +8,11 @@ def main() :
   
   main = py2cpp .Function( 'int', 'main', [ 'int argc', 'char * argv[]' ] )
   print str(main)
+  print '\n'.join( main.serialize() )
+    
+  main.set_body( 'return EXIT_SUCCESS ;' )
+  print str(main)
+  print '\n'.join( main.serialize() )
  
   return 0 
 
--- python/fps/py2cpp/Function.py
+++ python/fps/py2cpp/Function.py
@@ -3,7 +3,7 @@ from Variable import Variable
 
 class Function :
 
-  def __init__( self, return_type, name, args=[], body = None ) :
+  def __init__( self, return_type, name, args=[], attributes=[], body = None ) :
     if return_type is None or (isinstance( return_type, str ) and str == '') :
       raise Exception( "Function: Constructed with empty/null return type" ) 
     
@@ -13,31 +13,101 @@ class Function :
     if isinstance( return_type, str ) :
       return_type = Type( return_type ) 
 
-    self.__clear() 
-    self._name = name 
-    self._rt   = return_type 
-    self._args = args 
+    self.clear() 
+    self._name      = name 
+    self._rt        = return_type 
+    self._is_static = True if 'static' in attributes else False 
+    self._is_inline = True if 'inline' in attributes else False
+    self._is_const  = True if (not self._is_static and 'const' in attributes) else False 
+    self._is_proto  = True if body is None or body is '' else False
+
+    self._args      = []
+    for arg in args :
+      if isinstance( arg, str ) :
+        arg = Variable( arg ) 
+      if not isinstance( arg, Variable ) :
+        raise Exception( "Malformed argument to Function constructor '%s'", str(arg) )
+      self._args.append( arg )  
+    
     if body :
       self._body = body.split( '\n' ) 
     else :
       self._body = [] 
 
-  def __clear( self ) :
+  def clear( self ) :
     self._return = None 
     self._name   = None
     self._body   = []
     self._args   = []
 
-  def return_type( self ) :
-    return self._rt 
-  
-  def name( self ) :
-    return self._name 
-  
-  def args( self ) :
-    return self._args 
+  def set_body( self, body=None ) : 
+    if not body :
+      return self
+
+    if not isinstance( body, list ) :
+      self._body = [ body ] 
+    else :
+      self._body = body
+
+  def return_type ( self ) : return self._rt 
+  def name        ( self ) : return self._name 
+  def args        ( self ) : return self._args 
+  def body        ( self ) : return self._body
+  def is_inline   ( self ) : return self._is_inline 
+  def is_static   ( self ) : return self._is_static
+  def is_const    ( self ) : return self._is_const
+  def is_prototype( self ) : return self._is_prototype
     
   def __str__( self ) :
-    return 'Function : { name: %s, rt: %s, args: %s }'\
-           %( self.name(), self.return_type(), str(self.args()) )
+    args = [ arg.serialize() for arg in self._args ]
+    args = ', '.join( args )
+    return 'Function : { name: %s, rt: %s, args: [ %s ], const: %s, inline: %s, proto: %s }'\
+           %( self.name(), self.return_type(), args, self._is_const, self._is_inline, self._is_proto )
+
+  def serialize( self, indent=2 ) :
+    
+    f_indent  = ' ' * indent 
+    f_name    = self.name() 
+
+    f_prefix  = ''
+    f_prefix += 'inline ' if self.is_inline() else '' 
+    f_prefix += 'static ' if self.is_static() else ''
+    if len( f_prefix ) : f_prefix += ' ' 
+    
+    f_args    = []
+    for arg in self.args() :
+      f_args.append( arg.serialize() ) 
+    f_args = '' if not f_args or len(f_args) == 0 else ', '.join( f_args ) 
+
+    out = ''
+    if not self.body() or (len( self.body() ) == 0) :
+      out  = f_prefix + self.return_type().typename() + ' ' + f_name + '( ' + f_args + ' )' 
+      if self.is_const() :
+        out += ' const' 
+      out += ' ;'
+      return [ out ]
+
+    else :
+      out += f_prefix + self.return_type().typename() + ' ' + f_name + '( ' +  f_args + ' )'
+      
+      if self.is_const() :
+        out += ' const' 
+      
+      cpp  = [ out, '{' ]
+      cpp.extend( [ ((f_indent * 2) + x) for x in self.body() ] )
+      cpp.append( '}' )
+      
+      return cpp
+        
+      
+        
+      
+
+    
+    
+    
+  
+    
+
+    
   
