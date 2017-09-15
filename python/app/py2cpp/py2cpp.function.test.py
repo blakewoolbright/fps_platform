49e65d77776a1d65ca7799a232460e093377144b
Blake Woolbright <blake.woolbright@gmail.com>
2017-09-14 22:29:45 -0700

    Added prototype tests

--- python/app/py2cpp/py2cpp.function.test.py
+++ python/app/py2cpp/py2cpp.function.test.py
@@ -12,11 +12,17 @@ def main() :
                         )
     
   print "[ Default Indention ]" 
+  lines = [ py2cpp.Options.Indent_Width * ' ' + x for x in func.serialize( proto=True ) ]
+  print '\n'.join( lines )
   lines = [ py2cpp.Options.Indent_Width * ' ' + x for x in func.serialize() ]
   print '\n'.join( lines )
   print ""
 
   print "[ 2x Default Indention ]" 
+  lines  = func.serialize( py2cpp.Options.Indent_Width * 2, proto=True ) 
+  lines  = [ (py2cpp.Options.Indent_Width * ' ') + x for x in lines ] 
+  print '\n'.join( lines )
+
   lines  = func.serialize( py2cpp.Options.Indent_Width * 2 ) 
   lines  = [ (py2cpp.Options.Indent_Width * ' ') + x for x in lines ] 
   print '\n'.join( lines )
@@ -25,6 +31,8 @@ def main() :
   print "[ Tabbed Indention ]" 
   py2cpp.Options.Indent_Width = 1
   py2cpp.Options.Indent_Char  = '\t'
+  lines  = [ (py2cpp.Options.Indent_Width * '\t') + x for x in func.serialize( proto=True ) ] 
+  print '\n'.join( lines )
   lines  = [ (py2cpp.Options.Indent_Width * '\t') + x for x in func.serialize() ] 
   print '\n'.join( lines )
   print ""
@@ -33,11 +41,9 @@ def main() :
 
   print "[ Constructor for 'TestClass' ]"
   ctor = py2cpp.Constructor( 'TestClass', attrs=[ 'inline' ] )
-  lines  = [ (py2cpp.Options.Indent_Width * ' ') + x for x in ctor.serialize() ] 
-  print '\n'.join( lines )
-  print ""
-    
   ctor.set_body( "std::cout << \"TestClass\" << std::endl ;" ) 
+  lines  = [ (py2cpp.Options.Indent_Width * ' ') + x for x in ctor.serialize( proto=True ) ] 
+  print '\n'.join( lines )
   lines  = [ (py2cpp.Options.Indent_Width * ' ') + x for x in ctor.serialize() ] 
   print '\n'.join( lines )
   print ""
