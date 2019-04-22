2819613324aad6b17b78dbf6e7850b1c19460d18
Blake Woolbright <blake.woolbright@gmail.com>
2019-04-21 22:17:22 -0700

    Build fixes

--- external/cmake-modules/boost.cmake
+++ external/cmake-modules/boost.cmake
@@ -1,8 +1,7 @@
 fps_add_external_component( 
     LABEL                "boost"
     DESCRIPTION          "Boost C++ Libraries v1.56.0"
-    SYSTEM_INCLUDE_DIRS  "/home/bwoolbright/dev/fps_toolchain/boost/versions/boost-1.56.0/include"
-    SYSTEM_INCLUDE_DIRS  "/home/bwoolbright/dev/fps_toolchain/boost/versions/boost-1.56.0/include"
+    INCLUDE_DIRS         "/home/bwoolbright/dev/fps_toolchain/boost/versions/boost-1.56.0/include"
     REQUIRES             "libpthread.so"
     PROVIDES             "boost_system boost_program_options boost_filesystem boost_iostreams boost_regex boost_thread boost_date_time boost_serialization"
 )
---
+++ external/cmake-modules/python.cmake
@@ -0,0 +1,7 @@
+fps_add_external_component( 
+    LABEL                "python"
+    DESCRIPTION          "Python Devel Lib v3.6m"
+    INCLUDE_DIRS         "/usr/include/python3.6m"
+    REQUIRES             "libpthread.so"
+    PROVIDES             "python3.6m"
+)
